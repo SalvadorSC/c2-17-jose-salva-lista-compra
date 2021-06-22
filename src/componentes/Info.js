0040729d086bfde11a1171800feb21f3b0b796e8
Salvador Sánchez <sanchezcampossalvador@gmail.com>
2021-06-22 19:01:18 +0200

    enlaces entre paginas y botones preparados

--- src/componentes/Info.js
+++ src/componentes/Info.js
@@ -8,16 +8,21 @@ export const Info = (props) => {
   );
   const parametros = useParams();
   const history = useHistory();
-
   const { idItem } = props;
-  console.log(parametros);
   const volverAtras = (id) => {
     history.push(`/lista-articulo`);
+  };
+  const irCrearNuevoItem = (id) => {
+    history.push(`/formulario-articulo/`);
   };
   return (
     <section className="info espaciado bloque-superior">
       <i className="icono">
-        {idItem ? <FaMinusCircle onClick={volverAtras} /> : <FaPlusCircle />}
+        {idItem ? (
+          <FaMinusCircle onClick={volverAtras} />
+        ) : (
+          <FaPlusCircle onClick={irCrearNuevoItem} />
+        )}
       </i>
       <p className="n-articulos">
         {numeroArticulosComprados}/{numeroArticulos} comprados
